69ef0ae305b08d4d446c6e9a2c48656fcc1b3808
Santhosh Swamy V <138140952+santhoshswamyv@users.noreply.github.com>
2023-11-12 20:50:48 +0530

    Delete FileUploadModule directory

--- FileUploadModule/resources/views/error/error.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.layout')
-@section('content')
-    <div class="pb-5">
-        <h1 class="text-danger">Unexpected Error Occured..!!</h1>
-    </div>
-    <div class="pb-5">
-        <h2 class="text-info">Try Again...!!</h1>
-    </div>
-@endsection
